c996a2143695569f79e6be4ae5af64f73cd1159a
SoufianeGiGs <soufiane.otmane15@gmail.com>
2024-08-22 16:29:15 +0100

    commit2

--- app/Exports/BooksExport.php
+++ app/Exports/BooksExport.php
@@ -77,7 +77,7 @@ class BooksExport implements FromCollection, WithMapping, WithHeadings
         return [
             'ID',
             'Titre Propre',
-            'ISB',
+            'ISBN',
             'Titre Parallele',
             'Titre Auteur Different',
             'Complement Titre',
