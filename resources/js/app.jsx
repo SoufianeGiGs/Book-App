447840b306a45bd0e751fd1ce0fa8423bce094eb
SoufianeGiGs <soufiane.otmane15@gmail.com>
2024-08-23 00:26:11 +0100

    DockerCommit

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/inertia-react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '../css/app.css';
+import './Pages/Login'; // Add this temporarily
 
-createInertiaApp({ 
-  resolve: (name) => import(/* @vite-ignore */ `./Pages/${name}`).then((module) => module.default),
+
+createInertiaApp({
+  resolve: (name) => import(`./Pages/${name}.jsx`).then((module) => module.default),
   setup({ el, App, props }) {
     const root = ReactDOM.createRoot(el);
     root.render(
